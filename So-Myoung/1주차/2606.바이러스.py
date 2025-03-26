4f3078e471b88c1e799656ca272d63724cf5882f
So-Myoung <thaud9696@naver.com>
2025-03-26 11:29:27 +0900

    [#1]2606 바이러스

--- So-Myoung/1주차/2606.바이러스.py
+++ So-Myoung/1주차/2606.바이러스.py
@@ -1,15 +1,17 @@
 from collections import defaultdict
 
+
 def dfs(graph, visited, node):
     visited[node] = True
     count = 1
-    
+
     for v in graph[node]:
         if not visited[v]:
             count += dfs(graph, visited, v)
-    
+
     return count
-    
+
+
 n = int(input())
 connected = int(input())
 graph = defaultdict(list)
@@ -18,6 +20,6 @@ for _ in range(connected):
     v, e = map(int, input().split())
     graph[v].append(e)
     graph[e].append(v)
-    
-visited = [False] * (n+1)
-print(dfs(graph, visited, 1) - 1) # 1번 컴퓨터 제외
+
+visited = [False] * (n + 1)
+print(dfs(graph, visited, 1) - 1)  # 1번 컴퓨터 제외
