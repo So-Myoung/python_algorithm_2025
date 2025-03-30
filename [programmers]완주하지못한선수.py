610f848ae1dc62f6e925375d6799846e53c24c28
Jay kee <jkee@purdue.edu>
2025-03-30 16:25:42 +0900

    [#14] 프로그래머스 완주하지못한선수

---
+++ [programmers]완주하지못한선수.py
@@ -0,0 +1,24 @@
+# 내 풀이
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
+
+
+# 인상 깊었던 풀이
+# 각 key당 hash 값이 다른걸 이용한 풀이 
+def solution1(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
