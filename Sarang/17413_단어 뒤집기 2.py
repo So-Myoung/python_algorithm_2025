031f1b72f95a6b4e0cef9d9df01b34d67d8c9231
Sarang Kim <77392642+vvtkfkddl11@users.noreply.github.com>
2025-04-09 02:13:25 +0900

    [#18] 17413 단어 뒤집기 2

---
+++ Sarang/17413_단어 뒤집기 2.py
@@ -0,0 +1,40 @@
+import sys
+
+
+s = str(sys.stdin.readline().strip())
+
+def solution(strs):
+    answer = ""
+    arr = []
+    flag = 0
+    reverse_strs = ""
+    tag_strs = ""
+
+    for i in range(len(strs)):
+        if strs[i] == ">":
+            tag_strs += strs[i]
+            arr.append(tag_strs)
+            tag_strs = ""
+            flag = 0
+        elif strs[i] == "<" or flag == 1:
+            if reverse_strs:
+                arr.append(reverse_strs)
+                reverse_strs = ""
+            flag = 1
+            tag_strs += strs[i]
+        elif strs[i] == " ":
+            reverse_strs += " "
+            arr.append(reverse_strs)
+            reverse_strs = ""
+        elif i == (len(strs)-1):
+            reverse_strs = (strs[i] + reverse_strs)
+            arr.append(reverse_strs)
+        else:
+            reverse_strs = (strs[i] + reverse_strs)
+
+    for i in arr:
+        answer += i
+    return answer
+
+
+print(solution(s))
